16a6ccabaf736c4c4d8fa3c7848b01f61849c8ff
JustinDev <justinmandia25@gmail.com>
2026-06-19 23:42:47 +0800

    Fix the design of the Header

--- src/components/GenreFilter.jsx
+++ src/components/GenreFilter.jsx
@@ -1,7 +1,17 @@
+import { ChevronDown } from "lucide-react";
+import './GenreFilter.css'
+
 export function GenreFilter() {
   return (
     <>
-    <button>Genre</button>
+    <div className="genres-container" >
+      <button className="button-genres">
+        <span>Genres</span>
+        <ChevronDown className="genres-arrow" size={15}/>
+      </button>
+
+      {/* List of the Genres */}
+    </div>
     </>
   )
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,8 +4,10 @@ import './SearchBar.css'
 export function SearchBar() {
   return (
     <div className="search">
-      <input type="text" className='search-input' placeholder="Search Movie or Genre...." />
-      <SearchIcon />
+      <input type="text" className='search-input' placeholder="Search Movie or Genres..." />
+      <button className='search-button'>
+        <SearchIcon />
+      </button>
     </div>
   );
 }
