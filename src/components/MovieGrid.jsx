f301b8e49ac7930640635f99bb1fca4ee7cc8cad
JustinDev <justinmandia25@gmail.com>
2026-06-20 04:19:43 +0800

    Create a design for the Movie Grid

--- src/components/MovieGrid.jsx
+++ src/components/MovieGrid.jsx
@@ -1,7 +1,180 @@
+import { Plus, Star } from "lucide-react";
+import "./MovieGrid.css";
+
 export function MovieGrid() {
   return (
-    <>
-      <div>Movie Grid of the Movies</div>
-    </>
-  )
+    <div className="grid-container">
+      <div className="movie-container">
+        <div className="movie-grid">
+          <div className="movie-image-container">
+            <img
+              src="/images/movies/f1-the-movie.webp"
+              alt="F1 The Movie"
+              className="movie-image"
+            />
+            <span className="rating">
+              <Star size={18} className="rating-star" /> 4.5
+            </span>
+            <div className="add-to-my-list">
+              <Plus size={18} />
+            </div>
+          </div>
+          <div className="movie-title">F1 The Movie</div>
+        </div>
+
+        <div className="movie-grid">
+          <div className="movie-image-container">
+            <img
+              src="/images/movies/f1-the-movie.webp"
+              alt="F1 The Movie"
+              className="movie-image"
+            />
+            <span className="rating">
+              <Star size={18} className="rating-star" /> 4.5
+            </span>
+            <div className="add-to-my-list">
+              <Plus size={18} />
+            </div>
+          </div>
+          <div className="movie-title">F1 The Movie</div>
+        </div>
+
+        <div className="movie-grid">
+          <div className="movie-image-container">
+            <img
+              src="/images/movies/f1-the-movie.webp"
+              alt="F1 The Movie"
+              className="movie-image"
+            />
+            <span className="rating">
+              <Star size={18} className="rating-star" /> 4.5
+            </span>
+            <div className="add-to-my-list">
+              <Plus size={18} />
+            </div>
+          </div>
+          <div className="movie-title">F1 The Movie</div>
+        </div>
+
+        <div className="movie-grid">
+          <div className="movie-image-container">
+            <img
+              src="/images/movies/f1-the-movie.webp"
+              alt="F1 The Movie"
+              className="movie-image"
+            />
+            <span className="rating">
+              <Star size={18} className="rating-star" /> 4.5
+            </span>
+            <div className="add-to-my-list">
+              <Plus size={18} />
+            </div>
+          </div>
+          <div className="movie-title">F1 The Movie</div>
+        </div>
+
+        <div className="movie-grid">
+          <div className="movie-image-container">
+            <img
+              src="/images/movies/f1-the-movie.webp"
+              alt="F1 The Movie"
+              className="movie-image"
+            />
+            <span className="rating">
+              <Star size={18} className="rating-star" /> 4.5
+            </span>
+            <div className="add-to-my-list">
+              <Plus size={18} />
+            </div>
+          </div>
+          <div className="movie-title">F1 The Movie</div>
+        </div>
+
+        <div className="movie-grid">
+          <div className="movie-image-container">
+            <img
+              src="/images/movies/f1-the-movie.webp"
+              alt="F1 The Movie"
+              className="movie-image"
+            />
+            <span className="rating">
+              <Star size={18} className="rating-star" /> 4.5
+            </span>
+            <div className="add-to-my-list">
+              <Plus size={18} />
+            </div>
+          </div>
+          <div className="movie-title">F1 The Movie</div>
+        </div>
+
+        <div className="movie-grid">
+          <div className="movie-image-container">
+            <img
+              src="/images/movies/f1-the-movie.webp"
+              alt="F1 The Movie"
+              className="movie-image"
+            />
+            <span className="rating">
+              <Star size={18} className="rating-star" /> 4.5
+            </span>
+            <div className="add-to-my-list">
+              <Plus size={18} />
+            </div>
+          </div>
+          <div className="movie-title">F1 The Movie</div>
+        </div>
+
+        <div className="movie-grid">
+          <div className="movie-image-container">
+            <img
+              src="/images/movies/f1-the-movie.webp"
+              alt="F1 The Movie"
+              className="movie-image"
+            />
+            <span className="rating">
+              <Star size={18} className="rating-star" /> 4.5
+            </span>
+            <div className="add-to-my-list">
+              <Plus size={18} />
+            </div>
+          </div>
+          <div className="movie-title">F1 The Movie</div>
+        </div>
+
+        <div className="movie-grid">
+          <div className="movie-image-container">
+            <img
+              src="/images/movies/f1-the-movie.webp"
+              alt="F1 The Movie"
+              className="movie-image"
+            />
+            <span className="rating">
+              <Star size={18} className="rating-star" /> 4.5
+            </span>
+            <div className="add-to-my-list">
+              <Plus size={18} />
+            </div>
+          </div>
+          <div className="movie-title">F1 The Movie</div>
+        </div>
+
+        <div className="movie-grid">
+          <div className="movie-image-container">
+            <img
+              src="/images/movies/f1-the-movie.webp"
+              alt="F1 The Movie"
+              className="movie-image"
+            />
+            <span className="rating">
+              <Star size={18} className="rating-star" /> 4.5
+            </span>
+            <div className="add-to-my-list">
+              <Plus size={18} />
+            </div>
+          </div>
+          <div className="movie-title">F1 The Movie</div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,18 @@
 import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
 import { MovieGrid } from "../components/MovieGrid";
+import './HomePage.css'
 
 export function HomePage() {
   return (
     <div className="homepage-container">
       <Header />
+
+      <div className="movie-grid-container">
+        <span  className="heading-movie">Movies</span>
+
+        <MovieGrid />
+      </div>
     </div>
   )
 }
